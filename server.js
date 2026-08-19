f02e3b5c7c0012fe7335fe33ea7da319b571ec25
anna9331-cloud <anna9331@gmail.com>
2026-08-20 02:35:33 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,16 +9,13 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 메모리 내 별 데이터 저장소
 let stars = [];
 
 io.on('connection', (socket) => {
   console.log('클라이언트 연결됨:', socket.id);
 
-  // 접속 시 기존 별 목록 전달
   socket.emit('initStars', stars);
 
-  // 모바일 또는 PC에서 닉네임 제출 시
   socket.on('submitNickname', (nickname) => {
     const newStar = {
       id: Date.now() + Math.random().toString(36).substr(2, 4),
@@ -27,18 +24,15 @@ io.on('connection', (socket) => {
     };
     stars.push(newStar);
     
-    // 연결된 모든 전광판/모바일에 동시에 데이터 방송
+    // 단일 이벤트 전송
     io.emit('newStarAdded', newStar);
-    io.emit('newStar', newStar);
   });
 
-  // 개별 별 삭제
   socket.on('deleteStar', (starId) => {
     stars = stars.filter(s => s.id !== starId);
     io.emit('starDeleted', starId);
   });
 
-  // 전체 데이터 초기화
   socket.on('resetData', () => {
     stars = [];
     io.emit('resetStars');
